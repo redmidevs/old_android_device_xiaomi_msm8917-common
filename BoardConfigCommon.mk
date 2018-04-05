51dd85e830859fac632069a2b906c3f23a189d14
Offain <fabian.leutenegger@bluewin.ch>
2018-04-05 17:36:35 +0200

    msm8937-common: Get rid of recovery-only fstab
    Change-Id: Icd908cb8cdf1a764ee805e89a62a75bf161b3dbd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -216,7 +216,7 @@ PROTOBUF_SUPPORTED := true
 TARGET_RIL_VARIANT := caf
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/recovery/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/fstab.qcom
 
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
